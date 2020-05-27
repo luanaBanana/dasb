26816483ec84c7b1459e5be365984e7a1e44592f
luiXIV <luana.cusseddu.konto@gmail.com>
2020-05-27 23:35:27 +0200

    Last beauty things shiny App

--- ShinyApp/app.R
+++ ShinyApp/app.R
@@ -14,29 +14,29 @@ source("./Helper.R")
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Student Exam Marks"),
+    titlePanel("Student Exam Scores"),
     
     # Show a plot of the generated distribution
     mainPanel(
         tabsetPanel(
             tabPanel("Score Prediction", 
                 
-                selectInput(inputId = "parentEducation", label = "Choose your parents education", choices =  
+                selectInput(inputId = "parentEducation", label = "Choose your parents' education", choices =  
                                 c("associate's degree", "some college", "high school", "some high school", "bachelor's degree", "master's degree")),
                 
                 selectInput(inputId = "lunch", label = "Choose your lunch", choices = c("free/reduced", "standard")),
                 
-                selectInput(inputId = "prep", label = "Did you complete a test preparation cours", choice = c("completed", "none")),
+                selectInput(inputId = "prep", label = "Did you complete a test preparation course", choice = c("completed", "none")),
                 
                 selectInput(inputId = "gender", label = "Choose your gender", choices = c("male", "female")),
                 
-                selectInput(inputId = "race", label = "Select your race", choices = c("group A", "group B", "group C", "group D", "group E")),
+                selectInput(inputId = "race", label = "Select your ethnic group", choices = c("group A", "group B", "group C", "group D", "group E")),
                 textOutput("distPlot")
                 ), 
             tabPanel("Summary",                 selectInput(inputId = "type", label = "Which summary would you like to see", choices = c("writing", "reading", "maths")),
                 plotOutput("marksummary")), 
             
-            tabPanel("Parent Education", 
+            tabPanel("Comparing Parental Education", 
                 selectInput(inputId = "parentEducation2", label = "Choose your parents education", choices =  
                                      c("associate's degree", "some college", "high school", "some high school", "bachelor's degree", "master's degree")),
                 selectInput(inputId = "parentEducation3", label = "Choose your parents education", choices =  
@@ -51,18 +51,18 @@ ui <- fluidPage(
 server <- function(input, output) {
     output$distPlot <- renderText({ 
         result <- get_degree(input$parentEducation, input$lunch, input$prep, input$gender, input$race)
-        paste("The average score for you inputparameters is math ", result[1], "reading  ", result[2], "writing", result[3])
+        paste("The average score for you input parameters are: Math ", result[1], ", Reading  ", result[2], ", Writing", result[3], ".")
     })
     
     output$marksummary<-renderPlot({
         if (input$type == "writing"){
-            ggplot(studPerf, aes(studPerf$gender, studPerf$`writing score`, color = studPerf$`test preparation course`)) + 
+            ggplot(studPerf, aes(studPerf$gender, studPerf$`writing score`, color = `test preparation course`)) + 
                 geom_boxplot() + 
                 ggtitle("Writing scores by Gender Boxplot") + 
                 xlab("Gender") + 
                 ylab("Writing Scores")
         } else if (input$type == "reading"){
-            ggplot(studPerf, aes(studPerf$gender, studPerf$`reading score`, color = studPerf$`test preparation course`)) + 
+            ggplot(studPerf, aes(studPerf$gender, studPerf$`reading score`, color = `test preparation course`)) + 
                 geom_boxplot() + 
                 ggtitle("Reading scores by Gender Boxplot") + 
                 xlab("Gender") + 
@@ -83,11 +83,11 @@ server <- function(input, output) {
         # add a combined score for math, reading and writing
         filterStudPerf$score <- (filterStudPerf$`math score` + filterStudPerf$`reading score` + filterStudPerf$`writing score`) / 3
         # plot the result
-        ggplot(filterStudPerf, aes(filterStudPerf$`parental level of education`, filterStudPerf$`score`, color = filterStudPerf$`parental level of education`)) + 
+        ggplot(filterStudPerf, aes(filterStudPerf$`parental level of education`, filterStudPerf$`score`,color = filterStudPerf$`parental level of education`)) + 
             geom_boxplot() + 
             ggtitle("Scores by parental education Boxplot") + 
             xlab("Parent Education") + 
-            ylab("Scores")
+            ylab("Average Score")
     })
 
 }
